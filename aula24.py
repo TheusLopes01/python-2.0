05c158de9c1f7423dbe47fa83ef0875969fa5a6c
TheusLopes01 <mslopesilva01@gmail.com>
2024-02-26 21:13:24 -0300

    update

---
+++ aula24.py
@@ -0,0 +1,21 @@
+# Operadores in e not in
+# Strings são iteráveis
+# 0 1 2 3 4 5 6
+# M a t h e u s
+# -7-6-5-4-3-2-1
+
+#nome = 'Matheus'
+#print(nome[-7])
+#print(nome[2])
+#print('b' in nome)
+#print('a' in nome)
+#print('us' not in nome)
+#print('adi' not in nome)
+
+nome = input('Digite seu nome: ')
+encontrar = input('Digite o que deseja encontrar: ')
+
+if encontrar in nome:
+    print(f'{encontrar} está em {nome}')
+else:
+    print(f'{encontrar} não está em {nome}')
